040827f2cac2cfcaf99a00da6cba0942fa8cfa7b
shenmo <jifengshenmo@outlook.com>
2021-12-12 16:40:23 +0800

    修改：     build.sh 	修改：     debian/DEBIAN/postinst 	修改：     debian/DEBIAN/postrm 	新文件：   debian/opt/durapps/spark-store/bin/spark-dstore-patch 	修改：     debian/opt/durapps/spark-store/bin/spark-store 	修改：     debian/opt/durapps/spark-store/bin/ussinstall 	新文件：   debian/opt/durapps/spark-store/bin/ussremove

--- build.sh
+++ build.sh
@@ -5,14 +5,16 @@ cd `dirname $realpath`
 
 mkdir -p build && cd build
 
-/usr/lib/qt5/bin/qmake ../spark-store-project.pro -spec linux-g++ CONFIG+=qtquickcompiler && /usr/bin/make qmake_all
+/usr/lib/qt5/bin/qmake ../spark-store.pro -spec linux-g++ CONFIG+=qtquickcompiler && /usr/bin/make qmake_all
 make -j$(cat /proc/cpuinfo | grep "processor" | wc -l)
 
 cd ..
 
 cp ./ssinstall ./debian/opt/durapps/spark-store/bin/
+cp ./ussinstall ./debian/opt/durapps/spark-store/bin/
+cp ./ussremove ./debian/opt/durapps/spark-store/bin/
 cp ./spark-dstore-patch ./debian/opt/durapps/spark-store/bin/
-cp ./build/src/spark-store ./debian/opt/durapps/spark-store/bin/
+cp ./build/spark-store ./debian/opt/durapps/spark-store/bin/
 cp ./translations/*.qm ./debian/usr/share/spark-store/translations/
 
 version=$(cat ./debian/DEBIAN/control | grep "Version" | awk -F ' '  '{print $2}')
